922c949f4e0016f13fce8eff441eb713a9f32987
hbsnow <yuki.4uing@gmail.com>
2018-06-24 02:06:32 +0900

    カウンターを作った

---
+++ src/renderer/components/CounterSample.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="count()">Count: {{this.$store.state.Counter.main}}</button>
+</template>
+
+<script>
+  import SystemInformation from './LandingPage/SystemInformation'
+
+  export default {
+    name: 'counter-sample',
+    components: { SystemInformation },
+    methods: {
+      count () {
+        console.log(this.$store.dispatch('someAsyncTask'))
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
